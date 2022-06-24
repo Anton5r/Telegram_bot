c5d0aadf4fad38ab013da3b86405927ed0d4b5af
Anton5r <75029720+Anton5r@users.noreply.github.com>
2022-06-24 19:13:54 +0500

    Add files via upload

---
+++ install.py
@@ -0,0 +1,8 @@
+import os
+
+os.system(r'pip install --upgrade pip')
+os.system(r'pip install aiogram')
+os.system(r'pip install asyncio')
+os.system(r'pip install db-sqlite3')
+os.system(r'pip install teminal_banner')
+os.system(r'pip install logging')
